acd767f0e215a20b957c08b1ad51eab5c2e3d97d
openthestone <2269618999@qq.com>
2023-09-14 16:00:08 +0800

    code

---
+++ mini_redis/src/bin/client.rs
@@ -0,0 +1,103 @@
+use lazy_static::lazy_static;
+use mini_redis::FilterLayer;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use volo::FastStr;
+use volo_gen::mini_redis::{RedisRequest, RequestType};
+use mini_redis::{DEFAULT_ADDR,BASE_REQUEST};
+
+lazy_static! {
+    static ref CLIENT: volo_gen::mini_redis::RedisServiceClient = {
+        let addr: SocketAddr = DEFAULT_ADDR.parse().unwrap();
+        volo_gen::mini_redis::RedisServiceClientBuilder::new("redis")
+            .layer_outer(FilterLayer)
+            .address(addr)
+            .build()
+    };
+}
+#[volo::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let mut args: Vec<String> = std::env::args().collect();
+
+    let req = match args[1].to_lowercase().as_str() {
+        "set" => RedisRequest {
+            key: Some(FastStr::from(Arc::new(args.remove(2)))),
+            value: Some(FastStr::from(Arc::new(args.remove(2)))),
+            request_type: RequestType::Set,
+            expire_time: match args.len() {
+                4 => Some(args.remove(3).parse().unwrap()),
+                2 => None,
+                _ => panic!("invalid args"),
+            },
+            ..BASE_REQUEST.clone()
+        },
+        "get" => RedisRequest {
+            key: Some(FastStr::from(Arc::new(args.remove(2)))),
+            request_type: RequestType::Get,
+            ..BASE_REQUEST.clone()
+        },
+        "del" => RedisRequest {
+            key: Some(FastStr::from(Arc::new(args.remove(2)))),
+            request_type: RequestType::Del,
+            ..BASE_REQUEST.clone()
+        },
+        "ping" => RedisRequest { ..BASE_REQUEST.clone() },
+        "subscribe" => RedisRequest {
+            request_type: RequestType::Subscribe,
+            channels: Some(
+                args.drain(2..)
+                    .map(|x| FastStr::from(Arc::new(x)))
+                    .collect(),
+            ),
+            block: Some(false),
+            ..BASE_REQUEST.clone()
+        },
+        "publish" => RedisRequest {
+            value: Some(FastStr::from(Arc::new(args.remove(3)))),
+            request_type: RequestType::Publish,
+            channels: Some(vec![FastStr::from(Arc::new(args.remove(2)))]),
+            ..BASE_REQUEST.clone()
+        },
+        _ => {
+            panic!("unknown command");
+        }
+    };
+    let resp = CLIENT.redis_command(req.clone()).await;
+    match resp {
+        Ok(resp) => match resp.response_type {
+            volo_gen::mini_redis::ResponseType::Print => {
+                println!("{}", resp.value.unwrap())
+            }
+            volo_gen::mini_redis::ResponseType::Trap => {
+                println!(
+                    "subscribe {} channels",
+                    req.channels.as_ref().unwrap().len()
+                );
+                loop {
+                    let req = RedisRequest {
+                        block: Some(true),
+                        ..req.clone()
+                    };
+                    let resp = CLIENT.redis_command(req).await;
+                    match resp {
+                        Ok(info) => {
+                            println!("{}", info.value.unwrap());
+                        }
+                        _ => {
+                            println!("error");
+                        }
+                    }
+                }
+            }
+        },
+        Err(e) => match e {
+            volo_thrift::ResponseError::Application(err) => {
+                println!("{}", err.message)
+            }
+            _ => {
+                tracing::error!("{:?}", e);
+            }
+        },
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::net::SocketAddr;
+
+use axum::{
+    extract::{Path, State, Query},
+    http::StatusCode,
+    routing::get,
+    Router,
+    response,
+};
+use mini_redis::{DEFAULT_ADDR, FilterLayer, BASE_REQUEST};
+use serde::Deserialize;
+use std::sync::Arc;
+use volo_gen::mini_redis::{RedisServiceClient, RedisRequest,RequestType, RedisResponse};
+use faststr::FastStr;
+type RpcClient = RedisServiceClient;
+
+async fn get_response(req: RedisRequest, rpc_cli: &mut RpcClient) -> Option<FastStr> {
+    rpc_cli.redis_command(req).await.unwrap_or_else(|x|  RedisResponse{ 
+        value: Some(FastStr::from(match x{
+            volo_thrift::ResponseError::Application(err) => err.message,
+            _ => "Internal Server Error".to_string(),
+            })),
+        response_type: volo_gen::mini_redis::ResponseType::Print,
+    }).value
+}
+
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let addr: SocketAddr = DEFAULT_ADDR.parse().unwrap();
+    let rpc_cli =volo_gen::mini_redis::RedisServiceClientBuilder::new("redis")
+        .layer_outer(FilterLayer)
+        .address(addr)
+        .build();
+    let app = Router::new()
+        .route("/ping", get(ping))
+        .route("/get/:keys", get(get_key).with_state(rpc_cli.clone()))
+        .route(
+            "/set",
+            get(set_key).with_state(rpc_cli.clone()),
+        )
+        .route("/del/:keys", get(del_key).with_state(rpc_cli));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn ping() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "pong")
+}
+
+async fn get_key(Path(key): Path<String>, State(mut rpc_cli): State<RpcClient>) -> Result<response::Json<String>, StatusCode> {
+    let req = RedisRequest {
+        key: Some(FastStr::from(Arc::new(key))),
+        request_type: RequestType::Get,
+        ..BASE_REQUEST.clone()
+    };
+    match get_response(req, &mut rpc_cli).await {
+        Some(value) => match value.as_str(){
+            "nil" => Ok(response::Json("nil".to_string())),
+            _ => Ok(response::Json(value.into_string())),
+        },
+        None => Ok(response::Json("nil".to_string())),
+    }
+}
+
+#[derive(Deserialize)]
+struct KVPair {
+    key: String,
+    value: String,
+}
+
+async fn set_key(State(mut rpc_cli): State<RpcClient>, Query(params): Query<KVPair>) -> Result<response::Json<String>,StatusCode> {
+    let req = RedisRequest {
+        key: Some(FastStr::from(Arc::new(params.key))),
+        value: Some(FastStr::from(Arc::new(params.value))),
+        request_type: RequestType::Set,
+        ..BASE_REQUEST.clone()
+    };
+    match get_response(req, &mut rpc_cli).await {
+        Some(value) => Ok(response::Json(value.into_string())),
+        None => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+async fn del_key(
+    State(mut rpc_cli): State<RpcClient>,
+    Path(key): Path<String>,
+) -> Result<response::Json<String>, StatusCode> {
+    let req = RedisRequest {
+        key: Some(FastStr::from(Arc::new(key))),
+        request_type: RequestType::Del,
+        ..BASE_REQUEST.clone()
+    };
+    match get_response(req, &mut rpc_cli).await {
+        Some(value) => {
+            match value.as_str() {
+                "Ok" => Ok(response::Json(value.into_string())),
+                "nil" => Ok(response::Json("nil".to_string())),
+                _ => Err(StatusCode::INTERNAL_SERVER_ERROR),
+            }
+        },
+        None => Err(StatusCode::NOT_FOUND),
+    }
+}
